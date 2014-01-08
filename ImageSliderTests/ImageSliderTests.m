a07511cd73356aab28fb588424160560fc342296
steve0503 <leehr76@gmail.com>
2014-01-09 06:21:02 +0900

    Initial Commit

---
+++ ImageSliderTests/ImageSliderTests.m
@@ -0,0 +1,34 @@
+//
+//  ImageSliderTests.m
+//  ImageSliderTests
+//
+//  Created by 이 해용 on 2014. 1. 9..
+//  Copyright (c) 2014년 iamdreamer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ImageSliderTests : XCTestCase
+
+@end
+
+@implementation ImageSliderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
